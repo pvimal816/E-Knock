8ae92f910fdbb1f91f74dc6696bfffba32c352ce
pvimal816 <pvimal816@gmail.com>
2017-10-09 18:55:49 +0530

    removed unnecessary class.

--- src/nlp/MainClass.java
+++ src/nlp/MainClass.java
@@ -1,21 +1,24 @@
-/**
- *
+/**main class is for testing purpos only.
+ * Once this module to produce event class from sentence be ready to deploy
+ * we will use it with the android app to make our E-Knock.
  */
 package nlp;
 
 import java.util.Scanner;
 
 public class MainClass {
+    private static final String MESSAGE_TAGGER_MODEL_PATH = "models\\mainMessageTaggerModel.zip";
+    private static final String TOKENIZER_MODEL_PATH = "models\\en-token.bin";
 
     public static void main(String[] args) {
+
         System.out.println("Enter Sentence for tokenize : ");
         Scanner s = new Scanner(System.in);
         String sentence = s.nextLine();
         s.close();
 
-        PosTagger tagger = new PosTagger("models\\reminderModel.zip", "models\\en-token.bin");
+        PosTagger tagger = new PosTagger(MESSAGE_TAGGER_MODEL_PATH, TOKENIZER_MODEL_PATH);
         TokenizerUtils tokenizer = new TokenizerUtils("models\\en-token.bin");
-        //NameFinder nameFinder = new NameFinder("G:\\tools\\Opennlp models\\en-ner-person.zip","G:\\tools\\Opennlp models\\en-token.bin");
 
         String[] tokens = tokenizer.tokenizeStringUsingLearnableTokenizer(sentence);
         String[] tags = tagger.tagifyString(tokens);
@@ -25,20 +28,8 @@ public class MainClass {
             System.out.print(" " + tokens[i] + "_" + tags[i]);
 
         String eventString;
-        try {
-            eventString = EventExtractor.exractEvent(tokens, tags).toString();
-        } catch (NullPointerException e) {
-            eventString = "";
-            System.out.print("error in extracting events from message.");
-        }
+        eventString = new SentenceToEventExtractor(MESSAGE_TAGGER_MODEL_PATH, TOKENIZER_MODEL_PATH).extractEventFromString(sentence).toString();
         System.out.println("\n" + eventString);// + "Time : " + TimeParser.parseTime(eventString).toString());
-        String ss = "";
-        /*try {
-            ss = new SimpleDateFormat("dd,MMMMMMMMM").parse("1,October 2017").toString();
-        } catch (ParseException e) {
-            System.out.print(e.getMessage());
-        }
-        System.out.println(ss);*/
     }
 
 }
